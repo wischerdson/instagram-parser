587c46da0c7d6dd5871a72e706e7f04e48693a7b
Осмухин Даниил <wischerdson@gmail.com>
2023-02-09 20:57:04 +0300

    Настройка проекта

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
 	 */
 	public function register()
 	{
-		Sanctum::ignoreMigrations();
+
 	}
 
 	/**

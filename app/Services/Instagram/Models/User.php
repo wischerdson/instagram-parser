5061f39db96e19c987f130eeb93ae04b30be6ef1
Осмухин Даниил <wischerdson@gmail.com>
2023-02-11 22:44:54 +0300

    Получшение информация о пользователе instagram

---
+++ app/Services/Instagram/Models/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services\Instagram\Models;
+
+class User extends Model
+{
+	public ?string $whatsappNumber;
+
+	public ?string $externalUrl;
+
+	public ?string $biography;
+
+	public ?string $cityName;
+
+	public ?string $category;
+
+	public ?string $fullName;
+
+	public ?string $username;
+
+	public ?string $pk;
+
+	public ?string $publicPhoneNumber;
+
+	public ?int $publicPhoneCountryCode;
+
+	public ?string $publicEmail;
+
+	public ?string $contactPhoneNumber;
+
+	public ?string $addressStreet;
+
+	public ?bool $isBusiness;
+}
--- app/Services/Instagram/Responses/UserInfoResponse.php
+++ app/Services/Instagram/Responses/UserInfoResponse.php
@@ -2,7 +2,18 @@
 
 namespace App\Services\Instagram\Responses;
 
+use App\Services\Instagram\Models\User;
+use Illuminate\Support\Arr;
+
 class UserInfoResponse extends Response
 {
+	public readonly string $status;
 
+	public readonly User $user;
+
+	protected function fill(array $data): void
+	{
+		$this->status = Arr::get($data, 'status');
+		$this->user = new User(Arr::get($data, 'user', []));
+	}
 }
